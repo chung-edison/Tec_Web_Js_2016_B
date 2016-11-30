ad5c4d28bf1fae2f7dd387e7b477a2cefee40cfd
chung-edison <eddy2809@hotmail.com>
2016-11-30 14:36:43 -0500

    Usuario/:idUsuario req.params.idUsuario

--- ServidorHTTP/app.js
+++ ServidorHTTP/app.js
@@ -1,8 +1,40 @@
 var express = require('express')
 var app = express()
 
+var usuarios = [
+    {
+        id: 1,
+        nombre:'Pepe',
+        cedula:'1234567342'
+    },
+    {
+        id: 2,
+        nombre:'Carlos',
+        cedula:'1764567342'
+    },
+    {
+        id: 3,
+        nombre:'Juan',
+        cedula:'1234534342'
+    }
+]
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/Usuario', function (req, res) {
+    res.json(usuarios);
+})
+
+app.get('/Usuario/:idUsuario', function (req, res) { //http://localhost:3000/Usuario/2 
+    var idActual = req.params.idUsuario;
+    for(var i = 0; i < usuarios.length; i++){
+        if(idActual == usuarios[i].id) {
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe el Usuario')
 })
 
 app.get('/TecnologiasWeb', function (req, res) {
@@ -14,29 +46,27 @@ app.post('/TecnologiasWeb', function (req, res) {
     //request => req
     //response => res
     
+    var parametros = req.params
+    console.log(parametros);
+    
     var usuario = {
         nombre:'Edison',
         cedula:'0000000000'
     }
     
-    usuario.apellido = 'Chung';
-    usuario.mascotas = [];
-    usuario.casado = false;
-    
     res.append('token','1234');
+    res.json(usuario);
     
-    console.log('Lo que tengo en el request es:');
-    console.log(req);
-    console.log('Lo que tengo en el response es:');
-    console.log(res);
-
-    console.log('Cabeceras request:');
-    console.log(req.headers);
-    console.log('Cabeceras response:');
-    console.log(res.headers);
-    
-  res.json(usuario);
-  
+//    console.log('Lo que tengo en el request es:');
+//    console.log(req);
+//    console.log('Lo que tengo en el response es:');
+//    console.log(res);
+//
+//    console.log('Cabeceras request:');
+//    console.log(req.headers);
+//    console.log('Cabeceras response:');
+//    console.log(res.headers);
+      
   //no se pueden enviar multiples res.send()
 })
 
