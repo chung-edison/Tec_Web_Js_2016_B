dab8af93c7beff5cfc9bfd1aaa99e99a511453b9
chung-edison <eddy2809@hotmail.com>
2017-01-11 14:12:21 -0500

    crearUsuarioForm

--- ServidorHTTP/ProyectoSails/api/controllers/RutasController.js
+++ ServidorHTTP/ProyectoSails/api/controllers/RutasController.js
@@ -6,16 +6,18 @@
  */
 
 module.exports = {
-	home:function(req,res){
+    home: function (req, res) {
         //res.view(String: Nombre vista, Datos JSON)
-        return res.view('vistas/home',{
-            titulo:'Inicio',
-            numero:1,
-            mauricio:{
-                nombre:'Mauricio',
-                cedula:1718137159
+        return res.view('vistas/home', {
+            titulo: 'Inicio',
+            numero: 1,
+            mauricio: {
+                nombre: 'Mauricio',
+                cedula: 1718137159
             }
         });
+    },
+    crearUsuario: function (req, res) {
+        return res.view('vistas/crearUsuario');
     }
 };
-
--- ServidorHTTP/ProyectoSails/api/controllers/UsuarioController.js
+++ ServidorHTTP/ProyectoSails/api/controllers/UsuarioController.js
@@ -50,6 +50,41 @@ module.exports = {
         //        });
 
 
+    },
+
+    crearUsuarioForm: function (req, res) {
+        var parametros = req.allParams();
+        console.log(parametros);
+
+        if (req.method == 'POST') {
+
+            if (parametros.nombres && parametros.apellidos) {
+
+                Usuario.create({
+                    nombres: parametros.nombres,
+                    apellidos: parametros.apellidos,
+                    correo: parametros.correo
+                }).exec(function (err, usuarioCreado) {
+                    if (err) {
+                        return res.serverError(err)
+                    }
+                    sails.log.info(usuarioCreado);
+                    return res.view('vistas/home', {
+                        titulo: 'Inicio',
+                        numero: 1,
+                        mauricio: {
+                            nombre: 'Mauricio',
+                            cedula: 1718137159
+                        }
+                    });
+                });
+            } else {
+
+                return res.badRequest('No envía todos los parámetros.');
+            }
+        } else {
+            return res.badRequest('Método inválido.');
+        }
     }
 
 };
