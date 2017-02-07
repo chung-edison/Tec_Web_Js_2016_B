5d265d3947a323d1b765fb6569caf3e9811908b0
chung-edison <eddy2809@hotmail.com>
2017-02-07 15:10:11 -0500

    Crear usuario en la base de datos

--- Mascotas/api/controllers/UsuarioController.js
+++ Mascotas/api/controllers/UsuarioController.js
@@ -12,7 +12,37 @@ module.exports = {
     crearUsuario: function (req, res) {
 
         if (req.method == 'POST') {
-
+            var parametros = req.allParams();
+            if (parametros.nombres && parametros.apellidos) {
+                var usuarioCrear = {
+                    nombres: parametros.nombres,
+                    apellidos: paramentros.apellidos,
+                    correo: parametros.correo
+                }
+                if (parametros.correo == "") {
+                    delete usuarioCrear.correo;
+                }
+                Usuario.create(usuarioCrear).exec(function (err, usuarioCreado) {
+                    if (err) {
+                        return res.view('vistas/Error', {
+                            error: {
+                                descripcion: "Fallo al crear un usuario.",
+                                rawError: "err",
+                                url: "/CrearUsuario"
+                            }
+                        })
+                    }
+                    return res.view('vistas/Usuario/crearUsuario');
+                })
+            } else {
+                return res.view('vistas/Error', {
+                    error: {
+                        descripcion: "Llene todos los parámetros: nombres y apellidos.",
+                        rawError: "Fallo en envío de parámetros",
+                        url: "/CrearUsuario"
+                    }
+                });
+            }
         } else {
             return res.view('vistas/Error', {
                 error: {
